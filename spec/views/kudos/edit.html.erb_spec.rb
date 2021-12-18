c004c621d4469d24b869bdecf2115469595bab7f
Piotr <pio.witek@gmail.com>
2021-12-18 18:36:10 +0100

    added kudos

---
+++ spec/views/kudos/edit.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "kudos/edit", type: :view do
+  before(:each) do
+    @kudo = assign(:kudo, Kudo.create!(
+      title: "MyString",
+      content: "MyText",
+      employee: nil
+    ))
+  end
+
+  it "renders the edit kudo form" do
+    render
+
+    assert_select "form[action=?][method=?]", kudo_path(@kudo), "post" do
+
+      assert_select "input[name=?]", "kudo[title]"
+
+      assert_select "textarea[name=?]", "kudo[content]"
+
+      assert_select "input[name=?]", "kudo[employee_id]"
+    end
+  end
+end
---
+++ spec/views/kudos/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "kudos/index", type: :view do
+  before(:each) do
+    assign(:kudos, [
+      Kudo.create!(
+        title: "Title",
+        content: "MyText",
+        employee: nil
+      ),
+      Kudo.create!(
+        title: "Title",
+        content: "MyText",
+        employee: nil
+      )
+    ])
+  end
+
+  it "renders a list of kudos" do
+    render
+    assert_select "tr>td", text: "Title".to_s, count: 2
+    assert_select "tr>td", text: "MyText".to_s, count: 2
+    assert_select "tr>td", text: nil.to_s, count: 2
+  end
+end
