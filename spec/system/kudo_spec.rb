ccb7abaa88b37e8f7f47216eb6b4317e59d76855
Piotr <pio.witek@gmail.com>
2022-01-03 21:49:14 +0100

    fix: PR fixes and suggestions

--- spec/system/kudo_spec.rb
+++ spec/system/kudo_spec.rb
@@ -7,26 +7,15 @@ RSpec.describe 'Kudo test', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:employee2) { build(:employee, email: 'test2@test.com') }
-  let(:employee3) { build(:employee, email: 'test3@test.com') }
+  let!(:employee) { create(:employee) }
 
-  it 'Setup 2 employees and crud kudo' do
+  it 'crud kudo' do
     visit root_path
-    click_link 'Sign Up'
-    fill_in 'Email', with: employee2.email
-    fill_in 'Password', with: employee2.password
-    fill_in 'Password confirmation', with: employee2.password
-    click_button 'Sign up'
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
-    click_link 'Log Out'
-    expect(page).to have_content 'Log in'
-
-    click_link 'Sign Up'
-    fill_in 'Email', with: employee3.email
-    fill_in 'Password', with: employee3.password
-    fill_in 'Password confirmation', with: employee3.password
-    click_button 'Sign up'
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    click_link 'Log In'
+    fill_in 'Email', with: employee.email
+    fill_in 'Password', with: employee.password
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
 
     click_link 'New Kudo'
     fill_in 'Title', with: 'Title test1'
@@ -35,16 +24,14 @@ RSpec.describe 'Kudo test', type: :system do
 
     expect(page).to have_content 'Kudo was successfully created.'
     expect(page).to have_content 'Content Test1'
-    expect(page).to have_content 'test3@test.com'
 
     click_link 'Edit'
     fill_in 'Content', with: 'Another Content Test1'
     click_button 'Update Kudo'
     expect(page).to have_content 'Another Content Test1'
 
-    # accept_confirm do
     click_link 'Destroy'
-    # end
     expect(page).to have_content 'Kudo was successfully destroyed'
+    expect(page).not_to have_content 'Another Content Test1'
   end
 end
