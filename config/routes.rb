275e39a4de9578afe0ac1a9286d2c45f751f8ccb
Piotr <pio.witek@gmail.com>
2022-01-05 17:46:11 +0100

    feat: use devise and create a new model `AdminUser`

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   resources :kudos
-  devise_for :employees
+  devise_for :admin_users, path: 'admin_users'
+  devise_for :employees, path: 'employees'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'kudos#index'
 end
