36bff68e41cb3531fb7ffea4f4dfb526ba48ddc8
Piotr Witek <pio.witek@gmail.com>
2022-01-06 12:49:39 +0100

    fix: removed unnecesary controlers, updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@
 
 Rails.application.routes.draw do
   resources :kudos
-  devise_for :admin_users, path: 'admin_users'
+  devise_for :admin_users, path: 'admin'
   devise_for :employees, path: 'employees'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'kudos#index'
   get '/admin/', to: 'pages#dashboard'
 
-  namespace :admin_users do
+  namespace :admin_users, path: 'admin' do
     root to: 'pages#dashboard'
   end
 end
