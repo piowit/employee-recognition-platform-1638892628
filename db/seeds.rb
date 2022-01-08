2c1a3706ffda766a0882749a7b584f48de269d7e
Piotr Witek <pio.witek@gmail.com>
2022-01-08 15:29:25 +0100

    fix: minor changes seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,11 @@ employee4 = Employee.where(email: 'jhalpert@dm.com').first_or_create!(password: 
 employee5 = Employee.where(email: 'pbeesly@dm.com').first_or_create!(password: 'password')
 employee6 = Employee.where(email: 'cbratton@dm.com').first_or_create!(password: 'password')
 
-Kudo.create!(title: Faker::Verb.base, content: Faker::Marketing.buzzwords, employee_id: employee1.id, receiver_id: employee6.id)
-Kudo.create!(title: Faker::Verb.base, content: Faker::Marketing.buzzwords, employee_id: employee2.id, receiver_id: employee5.id)
-Kudo.create!(title: Faker::Verb.base, content: Faker::Marketing.buzzwords, employee_id: employee3.id, receiver_id: employee4.id)
-Kudo.create!(title: Faker::Verb.base, content: Faker::Marketing.buzzwords, employee_id: employee4.id, receiver_id: employee3.id)
-Kudo.create!(title: Faker::Verb.base, content: Faker::Marketing.buzzwords, employee_id: employee5.id, receiver_id: employee2.id)
-Kudo.create!(title: Faker::Verb.base, content: Faker::Marketing.buzzwords, employee_id: employee6.id, receiver_id: employee1.id)
+Kudo.create!(title: Faker::Hipster.word, content: Faker::Restaurant.review, employee_id: employee1.id, receiver_id: employee6.id)
+Kudo.create!(title: Faker::Hipster.word, content: Faker::Restaurant.review, employee_id: employee2.id, receiver_id: employee5.id)
+Kudo.create!(title: Faker::Hipster.word, content: Faker::Restaurant.review, employee_id: employee3.id, receiver_id: employee4.id)
+Kudo.create!(title: Faker::Hipster.word, content: Faker::Restaurant.review, employee_id: employee4.id, receiver_id: employee3.id)
+Kudo.create!(title: Faker::Hipster.word, content: Faker::Restaurant.review, employee_id: employee5.id, receiver_id: employee2.id)
+Kudo.create!(title: Faker::Hipster.word, content: Faker::Restaurant.review, employee_id: employee6.id, receiver_id: employee1.id)
 
 AdminUser.where(email: 'admin@admin.com').first_or_create!(password: 'password')
