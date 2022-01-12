a09a21db98386287a5e0ddb3d5e5b21205abc4f9
Piotr Witek <pio.witek@gmail.com>
2022-01-12 16:48:58 +0100

    fix: update rspec of giver and receiver of kudo in admin panel

--- spec/system/admin/admin_user_spec.rb
+++ spec/system/admin/admin_user_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe 'admin crud', type: :system do
     driven_by(:rack_test)
   end
 
-  let!(:employee) { create(:employee) }
+  let!(:employee1) { create(:employee) }
+  let!(:employee2) { create(:employee) }
   let!(:admin_user) { create(:admin_user) }
 
   it 'test admin crud actions' do
@@ -21,14 +22,21 @@ RSpec.describe 'admin crud', type: :system do
     click_link 'New Kudo'
     fill_in 'Title', with: 'test title1'
     fill_in 'Content', with: 'test content1'
+    select employee1.email, from: 'kudo[employee_id]'
+    select employee1.email, from: 'kudo[receiver_id]'
     click_button 'Create Kudo'
     expect(page).to have_content 'Kudo was successfully created.'
     expect(page).to have_content 'test title1'
+    expect(page).to have_content employee1.email
 
     click_link 'Edit'
     fill_in 'Title', with: 'test title2'
+    select employee2.email, from: 'kudo[employee_id]'
+    select employee2.email, from: 'kudo[receiver_id]'
     click_button 'Update Kudo'
     expect(page).to have_content 'test title2'
+    expect(page).to have_content employee2.email
+    expect(page).not_to have_content employee1.email
     click_link 'Back'
 
     click_link 'Destroy'
