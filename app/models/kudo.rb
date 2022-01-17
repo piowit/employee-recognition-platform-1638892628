96d3d26299d132bed27e21cf7bdb03f9fa6b43f9
Piotr Witek <pio.witek@gmail.com>
2022-01-17 21:11:52 +0100

    feat: add cv-kudo assosiation in model and controller

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Kudo < ApplicationRecord
-  validates :title, :content, :employee_id, :receiver_id, presence: true
+  validates :title, :content, :employee_id, :receiver_id, :company_value_id, presence: true
 
   belongs_to :giver_of_kudo, class_name: 'Employee', foreign_key: 'employee_id', inverse_of: :given_kudos
   belongs_to :receiver_of_kudo, class_name: 'Employee', foreign_key: 'receiver_id', inverse_of: :received_kudos
+  belongs_to :company_value, class_name: 'CompanyValue'
 end
