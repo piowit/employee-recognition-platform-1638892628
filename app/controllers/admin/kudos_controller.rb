fc484d3a2e2d44dc31d3305e3e55e98185eef631
Piotr Witek <pio.witek@gmail.com>
2022-01-17 21:29:01 +0100

    feat: admin kudo controller update

--- app/controllers/admin/kudos_controller.rb
+++ app/controllers/admin/kudos_controller.rb
@@ -55,7 +55,7 @@ module Admin
 
     # Only allow a list of trusted parameters through.
     def kudo_params
-      params.require(:kudo).permit(:title, :content, :employee_id, :receiver_id)
+      params.require(:kudo).permit(:title, :content, :employee_id, :receiver_id, :company_value_id)
     end
   end
 end
