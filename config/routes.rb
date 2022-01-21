9002f114bfd95a5eeaad3c38d90a5325fd82c845
Piotr Witek <pio.witek@gmail.com>
2022-01-21 16:19:44 +0100

    fix: added only to reward resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'kudos#index'
 
   resources :kudos
-  resources :rewards
+  resources :rewards, only: %i[index show]
 
   namespace :admin do
     root to: 'pages#dashboard'
