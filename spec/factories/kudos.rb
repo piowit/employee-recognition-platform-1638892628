93c2bcf9d537340250a7b4b4218e47505e1b8b5e
Piotr Witek <pio.witek@gmail.com>
2022-01-23 16:46:20 +0100

    feat: system spec updated for recived kudo count

--- spec/factories/kudos.rb
+++ spec/factories/kudos.rb
@@ -4,7 +4,8 @@ FactoryBot.define do
   factory :kudo do
     title { 'Title' }
     content { 'Content' }
-    giver_of_kudo
-    receiver_of_kudo
+    giver_of_kudo {}
+    receiver_of_kudo {}
+    company_value {}
   end
 end
