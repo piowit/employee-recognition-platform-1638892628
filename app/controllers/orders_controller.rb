7447c5d4ce5b59a8fc04feb49678fee074dc11f1
Piotr Witek <pio.witek@gmail.com>
2022-01-26 15:27:29 +0100

    feat: employee can buy rewards

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class OrdersController < ApplicationController
+  before_action :reward_find, only: [:create]
+
+  def index
+    @orders = Order.all
+  end
+
+  def create
+    if current_employee.points < @reward.price
+      redirect_to rewards_path, notice: 'You have insufficient funds'
+    else
+      @order = Order.new(employee: current_employee, reward: @reward)
+      if @order.save
+        redirect_to rewards_path, notice: 'Reward was bought'
+      else
+        redirect_to rewards_path, notice: 'Error'
+      end
+    end
+  end
+
+  private
+
+  def reward_find
+    @reward = Reward.find(params[:reward])
+  end
+end
