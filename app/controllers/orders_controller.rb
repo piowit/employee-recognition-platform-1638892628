1200eacdd7f09dd4e5bde849bab9620f243c05ba
Piotr Witek <pio.witek@gmail.com>
2022-01-28 19:33:13 +0100

    feat: implementation of Service Objects

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,15 +4,12 @@ class OrdersController < ApplicationController
   before_action :authenticate_employee!
 
   def create
-    if current_employee.points < reward.price
-      redirect_to rewards_path, notice: 'You have insufficient funds'
+    result = OrderCreator.new.call(employee: current_employee, reward: reward)
+
+    if result.success?
+      redirect_to rewards_path, notice: result.value!
     else
-      @order = Order.new(employee: current_employee, reward: reward)
-      if @order.save
-        redirect_to rewards_path, notice: 'Reward bought'
-      else
-        redirect_to rewards_path, notice: 'Error'
-      end
+      redirect_to rewards_path, notice: result.failure
     end
   end
 
