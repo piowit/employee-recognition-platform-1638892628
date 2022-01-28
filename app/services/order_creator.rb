d37c1b564b7ba6a4a6ebf5c08d2935bba4bcabc4
Piotr Witek <pio.witek@gmail.com>
2022-01-28 19:33:13 +0100

    feat: implementation of Service Objects

---
+++ app/services/order_creator.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+
+class OrderCreator
+  include Dry::Monads[:result, :do]
+
+  def call(employee:, reward:)
+    Order.transaction do
+      yield check_funds(employee, reward)
+
+      Success('Reward bought')
+    end
+  end
+
+  private
+
+  def check_funds(employee, reward)
+    if employee.points < reward.price
+      Failure('You have insufficient funds')
+    else
+      Success(create_order(employee, reward))
+    end
+  end
+
+  def create_order(employee, reward)
+    order = Order.new(employee: employee, reward: reward)
+    if order.save
+      Success()
+    else
+      Failure('Error')
+    end
+  end
+end
