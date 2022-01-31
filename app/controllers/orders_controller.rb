344401abde9c74af98e8e9527383282f5519ab9a
Piotr Witek <pio.witek@gmail.com>
2022-01-31 13:06:26 +0100

    Revert "feat: implementation of Service Objects"
    This reverts commit d37c1b564b7ba6a4a6ebf5c08d2935bba4bcabc4.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,15 @@ class OrdersController < ApplicationController
   before_action :authenticate_employee!
 
   def create
-    result = OrderCreator.new.call(employee: current_employee, reward: reward)
-
-    if result.success?
-      redirect_to rewards_path, notice: result.value!
+    if current_employee.points < reward.price
+      redirect_to rewards_path, notice: 'You have insufficient funds'
     else
-      redirect_to rewards_path, notice: result.failure
+      @order = Order.new(employee: current_employee, reward: reward)
+      if @order.save
+        redirect_to rewards_path, notice: 'Reward bought'
+      else
+        redirect_to rewards_path, notice: 'Error'
+      end
     end
   end
 
