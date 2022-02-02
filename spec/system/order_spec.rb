83eae5c56649ede6d56bff8ee9ecdb5c4c77b75f
Piotr Witek <pio.witek@gmail.com>
2022-02-02 19:11:45 +0100

    feat: rspec for orders list

---
+++ spec/system/order_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Order spec', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  let!(:employee) { create(:employee) }
+  let!(:reward) { create(:reward, price: 1) }
+  let!(:company_value) { create(:company_value) }
+
+  it 'tests order' do
+    # visit root_path
+    create(:kudo, giver: employee, receiver: employee, company_value: company_value)
+    login_as employee, scope: :employee
+
+    visit orders_path
+    expect(page).not_to have_content reward.title
+    expect(page).not_to have_content reward.description
+    expect(page).not_to have_content reward.price
+
+    visit rewards_path
+    expect(page).to have_content 'Received Points: 1'
+    expect(page).to have_content reward.title
+    click_link 'Buy'
+    expect(page).to have_content 'Reward bought'
+
+    visit orders_path
+    expect(page).to have_content reward.title
+    expect(page).to have_content reward.description
+    expect(page).to have_content reward.price
+  end
+end
