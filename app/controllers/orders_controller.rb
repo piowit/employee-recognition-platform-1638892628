363afc828367a8f2be25dc8326bf1d0d4e3f12b9
Piotr Witek <pio.witek@gmail.com>
2022-02-02 18:48:01 +0100

    feat: add orders list with snapshot

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_employee!
 
+  def index
+    @orders = Order.where(employee: current_employee)
+  end
+
   def create
     if current_employee.points < reward.price
       redirect_to rewards_path, notice: 'You have insufficient funds'
     else
-      @order = Order.new(employee: current_employee, reward: reward)
+      @order = Order.new(employee: current_employee, reward: reward, snapshot: reward)
       if @order.save
         redirect_to rewards_path, notice: 'Reward bought'
       else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_123547) do
+ActiveRecord::Schema.define(version: 2022_02_02_162927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_123547) do
     t.bigint "reward_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "snapshot"
     t.index ["employee_id"], name: "index_orders_on_employee_id"
     t.index ["reward_id"], name: "index_orders_on_reward_id"
   end
