ece3c8a1e7e4cbe6bdcfe30d45e65e4b51509767
Piotr Witek <pio.witek@gmail.com>
2022-02-04 15:51:56 +0100

    feat: fixing n+1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_employee!
 
   def index
-    @orders = Order.where(employee: current_employee)
+    @orders = Order.where(employee: current_employee).includes(:employee)
   end
 
   def create
