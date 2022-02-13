96794312de2d5c9154119c6eefcd96304dfa0092
Piotr Witek <pio.witek@gmail.com>
2022-02-13 20:39:57 +0100

    feat: add delivery status and deliver button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,7 +5,21 @@ module Admin
     before_action :authenticate_employee!
 
     def index
-      @orders = Order.includes(:employee)
+      @orders = Order.includes(:employee).order(:delivered)
+    end
+
+    def deliver
+      @order = Order.find(params[:id])
+      if @order.delivered 
+        redirect_to admin_orders_path, notice: 'Order already delivered'
+      else
+        @order.delivered = true
+        if @order.save
+          redirect_to admin_orders_path, notice: 'Order delivered'
+        else
+          redirect_to admin_orders_path, notice: 'Error'
+        end
+      end
     end
   end
 end
