cd38f329c1f0abb887de9ebd8814b7c2cf3dccdf
Piotr Witek <pio.witek@gmail.com>
2022-03-01 11:41:28 +0100

    fix: another update of smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  password: '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',  # ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-  domain: 'localhost',
+  password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+  domain: 'employeerecognitionplatfrom.heroku.com',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
