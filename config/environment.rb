77f4815187c61a44f18f4e5448aeda3ff5667279
Piotr Witek <pio.witek@gmail.com>
2022-03-02 17:25:32 +0100

    fix: rails credentials implement

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,8 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+  # password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+  password: Rails.application.credentials.dig(:sendgrid, :api_key), # This is the secret sendgrid API key which was issued during API key creation
   domain: 'employeerecognitionplatfrom.heroku.com',
   address: 'smtp.sendgrid.net',
   port: 587,
