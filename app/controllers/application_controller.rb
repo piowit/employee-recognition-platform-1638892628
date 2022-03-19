3d87a1aa99bc031424059cfa8068ae7fcbb7e6bf
Piotr Witek <pio.witek@gmail.com>
2022-03-19 09:12:16 +0100

    feat: added unit and system specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@
 
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
-
-  rescue_from Pundit::NotAuthorizedError, with: :timelimit_for_edit
-
-  private
-
-  def timelimit_for_edit
-    flash[:notice] = 'You can edit or delete within 5 min from creation.'
-    redirect_to(request.referer || root_path)
-  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'support/devise'
+require 'pundit/rspec'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -38,7 +39,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
-
+  config.include ActiveSupport::Testing::TimeHelpers  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
