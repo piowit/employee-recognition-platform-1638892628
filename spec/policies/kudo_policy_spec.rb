5804b305171f46a841985d2c82da25a96b4a7174
Piotr Witek <pio.witek@gmail.com>
2022-03-24 20:17:15 +0100

    fix: changed kudo policy spec for more readability

--- spec/policies/kudo_policy_spec.rb
+++ spec/policies/kudo_policy_spec.rb
@@ -10,18 +10,22 @@ RSpec.describe KudoPolicy do
   let(:kudo) { create(:kudo, giver: employee) }
 
   permissions :edit?, :update? do
-    it 'grants access if good employee and edit time less then 5 minutes' do
-      expect(subject).to permit(employee, kudo)
+    context 'when employee is kudo giver' do
+      it 'grants access if kudo is less then 5 minutes old' do
+        expect(subject).to permit(employee, kudo)
+      end
+
+      it 'denies access if kudo is over 5 minute old' do
+        kudo_old = create(:kudo, giver: employee)
+        travel 6.minutes do
+          expect(subject).not_to permit(employee, kudo_old)
+        end
+      end
     end
 
-    it 'denies access if wrong employee' do
-      expect(subject).not_to permit(employee_wrong, kudo)
-    end
-
-    it 'denies access if post is over 5 minute old' do
-      kudo_old = create(:kudo, giver: employee)
-      travel 6.minutes do
-        expect(subject).not_to permit(employee, kudo_old)
+    context 'when employee is not kudo giver' do
+      it 'denies access' do
+        expect(subject).not_to permit(employee_wrong, kudo)
       end
     end
   end
