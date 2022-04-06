c48450d28664e23c45d0510f6523302021bdcbc6
Piotr Witek <pio.witek@gmail.com>
2022-04-06 15:51:25 +0200

    feat: added categiores for rewards

--- app/controllers/admin/rewards_controller.rb
+++ app/controllers/admin/rewards_controller.rb
@@ -16,19 +16,33 @@ module Admin
 
     def create
       @reward = Reward.new(reward_params)
-      if @reward.save
-        redirect_to admin_rewards_path, notice: 'Reward was successfully created.'
-      else
-        render :new
+      begin
+        ActiveRecord::Base.transaction do
+          @reward.update!(reward_params)
+          @reward.categories.clear
+          params[:reward][:category_ids]&.each do |category_id|
+            @reward.categories << Category.find(category_id)
+          end
+          redirect_to admin_rewards_path, notice: 'Reward was successfully created.'
+        end
+      rescue ActiveRecord::RecordInvalid => e
+        render :new, notice: e.message
       end
     end
 
     def update
       @reward = Reward.find(params[:id])
-      if @reward.update(reward_params)
-        redirect_to admin_rewards_path, notice: 'Reward was successfully updated.'
-      else
-        render :edit
+      begin
+        ActiveRecord::Base.transaction do
+          @reward.update!(reward_params)
+          @reward.categories.clear
+          params[:reward][:category_ids]&.each do |category_id|
+            @reward.categories << Category.find(category_id)
+          end
+          redirect_to admin_rewards_path, notice: 'Reward was successfully updated.'
+        end
+      rescue ActiveRecord::RecordInvalid => e
+        render :edit, notice: e.message
       end
     end
 
@@ -41,7 +55,7 @@ module Admin
     private
 
     def reward_params
-      params.require(:reward).permit(:title, :description, :price)
+      params.require(:reward).permit(:title, :description, :price, :category_ids)
     end
   end
 end
