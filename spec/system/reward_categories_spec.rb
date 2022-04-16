7b77516d8f5d9a80020479c15a6a5dc058443d7f
Piotr Witek <pio.witek@gmail.com>
2022-04-16 08:21:32 +0200

    rewrite rewards categories spec

--- spec/system/reward_categories_spec.rb
+++ spec/system/reward_categories_spec.rb
@@ -5,56 +5,42 @@ require 'rails_helper'
 RSpec.describe 'Reward categories', type: :system do
   let!(:cat_rew1) { create(:category_reward) }
   let!(:cat_rew2) { create(:category_reward) }
-  let!(:cat_rew3) { create(:category_reward) }
-  let!(:cat_rew4) { create(:category_reward) }
   let!(:employee) { create(:employee) }
 
-  context 'when we want to check reward categories' do
-    before do
-      login_as employee, scope: :employee
-      visit root_path
-      click_link 'Rewards'
-    end
+  before do
+    login_as employee, scope: :employee
+    visit root_path
+    click_link 'Rewards'
+  end
 
-    it 'checks if category buttons exist' do
+  context 'when visiting rewards index page without a category selected' do
+    it 'displays all category buttons' do
       expect(page).to have_content cat_rew1.category.title
       expect(page).to have_content cat_rew2.category.title
-      expect(page).to have_content cat_rew3.category.title
-      expect(page).to have_content cat_rew4.category.title
-      expect(page).to have_content cat_rew1.reward.title
-      expect(page).to have_content cat_rew2.reward.title
-      expect(page).to have_content cat_rew3.reward.title
-      expect(page).to have_content cat_rew4.reward.title
     end
 
-    it 'checks if default all rewards visible' do
+    it 'displays all rewards' do
       expect(page).to have_content cat_rew1.reward.title
       expect(page).to have_content cat_rew2.reward.title
-      expect(page).to have_content cat_rew3.reward.title
-      expect(page).to have_content cat_rew4.reward.title
     end
 
-    it 'checks filtered rewards' do
-      # checking filter 1
-      click_link cat_rew1.category.title
-      expect(page).to have_content cat_rew1.reward.title
-      expect(page).not_to have_content cat_rew2.reward.title
-      expect(page).not_to have_content cat_rew3.reward.title
-      expect(page).not_to have_content cat_rew4.reward.title
+    context 'when a category is selected' do
+      it 'displays only categorized rewards' do
+        # checking filter 1
+        click_link cat_rew1.category.title
+        expect(page).to have_content cat_rew1.reward.title
+        expect(page).not_to have_content cat_rew2.reward.title
 
-      # checking filter 4
-      click_link cat_rew4.category.title
-      expect(page).not_to have_content cat_rew1.reward.title
-      expect(page).not_to have_content cat_rew2.reward.title
-      expect(page).not_to have_content cat_rew3.reward.title
-      expect(page).to have_content cat_rew4.reward.title
+        # checking filter 2
+        click_link cat_rew2.category.title
+        expect(page).not_to have_content cat_rew1.reward.title
+        expect(page).to have_content cat_rew2.reward.title
 
-      # checking default All filter
-      click_link 'All'
-      expect(page).to have_content cat_rew1.reward.title
-      expect(page).to have_content cat_rew2.reward.title
-      expect(page).to have_content cat_rew3.reward.title
-      expect(page).to have_content cat_rew4.reward.title
+        # checking default All filter
+        click_link 'All'
+        expect(page).to have_content cat_rew1.reward.title
+        expect(page).to have_content cat_rew2.reward.title
+      end
     end
   end
 end
