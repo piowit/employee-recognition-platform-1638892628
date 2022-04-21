32a463a15323a37a26453be6783fbd2fd32c6a33
Piotr Witek <pio.witek@gmail.com>
2022-04-21 15:42:40 +0200

    feat: export admin orders as csv

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 module Admin
   class OrdersController < AdminController
     def index
       @orders = Order.includes(:employee).order(:delivered)
+    end
+
+    def export
+      @orders = Order.order(:delivered)
+
+      respond_to do |format|
+        format.csv do
+          response.headers['Content-Type'] = 'text/csv'
+          response.headers['Content-Disposition'] = 'attachment; filename=orders.csv'
+          render template: 'admin/orders/export.csv.erb'
+        end
+      end
     end
   end
 end
