d03a15c46d1cc6cb032893696327289cb5a45c1d
Piotr Witek <pio.witek@gmail.com>
2022-05-23 16:45:22 +0200

    fix: destroy employee by admin fixed
    fixed Admin::EmployeesController destroy method, changed employee model to delete orders of employee, when employee is deleted

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,7 +6,7 @@ class Employee < ApplicationRecord
 
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id', dependent: :destroy, inverse_of: :giver
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
-  has_many :orders, dependent: :nullify
+  has_many :orders, dependent: :destroy
   has_many :rewards, through: :orders
 
   validates :first_name, :last_name, presence: true
