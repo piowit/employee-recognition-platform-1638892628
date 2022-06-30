d6cf2ff56073bb6bb50cf02386f9ace58b382c47
Piotr Witek <pio.witek@gmail.com>
2022-06-30 14:05:34 +0200

    style: rubocop fix

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class OrderForm
   include ActiveModel::Model
 
--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :address do
     employee
     sequence(:street) { |i| "Polna #{i}" }
     sequence(:postcode) { |i| "00-#{format('%03d', i)}" }
     sequence(:city) { |i| "Warszawa#{i}" }
+    last_used { Time.current }
   end
 end
