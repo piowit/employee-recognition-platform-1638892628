24595182e8dc245b2024955ed125ed90e9a1668e
Piotr Witek <p.witek@visuality.pl>
2022-07-07 14:57:12 +0200

    test: test for online_code, post/online orders

---
+++ spec/system/admin/admin_online_code_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'tempfile'
+require 'csv'
+
+RSpec.describe 'Online Code spec', type: :system do
+  let!(:employee) { create(:employee) }
+  let!(:admin) { create(:admin_user) }
+  let!(:reward_online) { create(:reward, delivery_method: 'online') }
+  let!(:reward_post) { create(:reward, delivery_method: 'post') }
+
+  it 'crud online code for online reward' do
+    login_as admin, scope: :admin_user
+    visit root_path
+    click_link 'Admin'
+    click_link 'Online Codes'
+
+    # add empty code
+    click_link 'Add Online Code'
+    click_button 'Create Online code'
+    expect(page).to have_content "Code can't be blank"
+
+    # add unique code
+    fill_in 'online_code[code]', with: '123-123'
+    select reward_online.title, from: 'online_code[reward_id]'
+    click_button 'Create Online code'
+    expect(page).to have_content 'Online code was successfully added.'
+    expect(page).to have_content '123-123'
+
+    # add not-unique code
+    click_link 'Add Online Code'
+    fill_in 'online_code[code]', with: '123-123'
+    select reward_online.title, from: 'online_code[reward_id]'
+    click_button 'Create Online code'
+    expect(page).to have_content 'Code has already been taken'
+
+    # edit code
+    click_link 'Online Codes'
+    click_link 'Edit'
+    fill_in 'online_code[code]', with: '333-333'
+    click_button 'Update Online code'
+    expect(page).to have_content 'Online code was successfully updated.'
+    expect(page).not_to have_content '123-123'
+    expect(page).to have_content '333-333'
+
+    # destroy code
+    click_link 'Online Codes'
+    click_link 'Destroy'
+    expect(page).to have_content 'Online code was successfully destroyed.'
+    expect(page).not_to have_content '333-333'
+  end
+
+  it 'exports csv with all online_codes fields' do
+    online_code = create(:online_code, reward: reward_online)
+    login_as admin, scope: :admin_user
+    visit root_path
+    click_link 'Admin'
+    click_link 'Online Codes'
+    click_on 'Download Online Codes as CSV'
+
+    csv = CSV.new(page.body).read
+
+    expect(csv).to have_content reward_online.slug
+    expect(csv).to have_content online_code.code
+  end
+end
--- spec/system/order_spec.rb
+++ spec/system/order_spec.rb
@@ -4,31 +4,83 @@ require 'rails_helper'
 
 RSpec.describe 'Order spec', type: :system do
   let!(:employee) { create(:employee) }
-  let!(:reward) { create(:reward, price: 1, available_items: 1) }
+  let!(:reward_post) { create(:reward, price: 1, available_items: 1, delivery_method: 'post') }
+  let!(:reward_online) { create(:reward, price: 1, available_items: 1, delivery_method: 'online') }
+  let!(:online_code) { create(:online_code, reward: reward_online) }
   let!(:company_value) { create(:company_value) }
-  let!(:online_code) { create(:online_code, reward: reward) }
 
-  it 'tests order' do
+  before do
     online_code
     create(:kudo, giver: employee, receiver: employee, company_value: company_value)
     login_as employee, scope: :employee
+  end
 
+  it 'tests online order' do
     visit orders_path
-    expect(page).not_to have_content reward.title
-    expect(page).not_to have_content reward.description
-    expect(page).not_to have_content reward.price
+    expect(page).not_to have_content reward_online.title
+    expect(page).not_to have_content reward_online.description
+    expect(page).not_to have_content reward_online.price
 
     visit rewards_path
     expect(page).to have_content 'Received Points: 1'
-    expect(page).to have_content reward.title
-    click_link 'Buy'
-    expect(page).to have_content reward.title
+    expect(page).to have_content reward_online.title
+    reward_online_row = page.find("tr[data-reward-id=\"#{reward_online.id}\"]")
+    reward_online_row.click_link 'Buy'
+    expect(page).to have_content reward_online.title
     click_button 'Buy'
     expect(page).to have_content 'Reward bought'
 
     visit orders_path
-    expect(page).to have_content reward.title
-    expect(page).to have_content reward.description
-    expect(page).to have_content reward.price
+    expect(page).to have_content reward_online.title
+    expect(page).to have_content reward_online.description
+    expect(page).to have_content reward_online.price
+  end
+
+  it 'tests post order with adding new address' do
+    visit orders_path
+    expect(page).not_to have_content reward_post.title
+    expect(page).not_to have_content reward_post.description
+    expect(page).not_to have_content reward_post.price
+
+    visit rewards_path
+    expect(page).to have_content 'Received Points: 1'
+    expect(page).to have_content reward_post.title
+    reward_post_row = page.find("tr[data-reward-id=\"#{reward_post.id}\"]")
+    reward_post_row.click_link 'Buy'
+    expect(page).to have_content reward_post.title
+    select 'Add new address', from: 'order_form[address_id]'
+    fill_in 'order_form[street]', with: 'Odolanska 56'
+    fill_in 'order_form[postcode]', with: '02-562'
+    fill_in 'order_form[city]', with: 'Warszawa'
+    click_button 'Buy'
+    expect(page).to have_content 'Reward bought'
+
+    visit orders_path
+    expect(page).to have_content reward_post.title
+    expect(page).to have_content reward_post.description
+    expect(page).to have_content reward_post.price
+  end
+
+  it 'tests post order with using old address' do
+    address = create(:address, employee: employee)
+    visit orders_path
+    expect(page).not_to have_content reward_post.title
+    expect(page).not_to have_content reward_post.description
+    expect(page).not_to have_content reward_post.price
+
+    visit rewards_path
+    expect(page).to have_content 'Received Points: 1'
+    expect(page).to have_content reward_post.title
+    reward_post_row = page.find("tr[data-reward-id=\"#{reward_post.id}\"]")
+    reward_post_row.click_link 'Buy'
+    expect(page).to have_content reward_post.title
+    select address.full_address, from: 'order_form[address_id]'
+    click_button 'Buy'
+    expect(page).to have_content 'Reward bought'
+
+    visit orders_path
+    expect(page).to have_content reward_post.title
+    expect(page).to have_content reward_post.description
+    expect(page).to have_content reward_post.price
   end
 end
