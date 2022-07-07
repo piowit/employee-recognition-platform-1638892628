252c6f516544c6d1b8f3f33b04ab37dbc9a7ce1b
Piotr Witek <pio.witek@gmail.com>
2022-07-07 07:11:29 +0200

    feat: add counter_culture and simplecov-rails gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,8 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'support/devise'
 require 'pundit/rspec'
-
+require 'simplecov'
+SimpleCov.start
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
