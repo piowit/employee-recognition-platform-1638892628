9002917386f8cc4c6add02d14a60a55caae98276
Piotr Witek <p.witek@visuality.pl>
2022-07-18 09:28:56 +0200

    refactor: remove unneccessary method from online_code model, added overcommit gem

--- app/models/online_code.rb
+++ app/models/online_code.rb
@@ -7,26 +7,11 @@ class OnlineCode < ApplicationRecord
   validates :code, :reward, presence: true
   validates :code, uniqueness: true
 
-  counter_culture :reward, column_name: proc { |model| model.sent? ? nil : 'online_codes_count' }
+  counter_culture :reward, column_name: proc { |online_code| online_code.sent? ? nil : 'online_codes_count' }
 
   scope :available, -> { where(order_id: nil) }
 
   def sent?
     order.present?
-  end
-
-  def self.import(file)
-    ActiveRecord::Base.transaction do
-      CSV.foreach(file.path, headers: true) do |row|
-        online_code_hash = row.to_hash
-        next if online_code_hash['slug'].nil?
-        next if OnlineCode.where(code: online_code_hash['code']).present?
-        next if Reward.where(slug: online_code_hash['slug']).blank?
-
-        reward = Reward.where(slug: online_code_hash['slug']).first
-        online_code = OnlineCode.new(code: online_code_hash['code'], reward_id: reward.id)
-        online_code.save!
-      end
-    end
   end
 end
